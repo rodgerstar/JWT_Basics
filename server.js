4edce091f4c0ed1075c0252e51f9aa2ca2a522e3
Rodgers Okwemba <rodgerstarrodgers@gmail.com>
2025-10-10 15:43:25 +0300

    route handlers and chaining route handlers

--- server.js
+++ server.js
@@ -18,6 +18,31 @@ app.get(['/old-page', '/old-page.html'], (req, res) => {
     res.redirect(301, '/new-page.html')
 })
 
+//Route Handlers
+app.get(['/hello', '/hello.html'], (req, res, next) => {
+    console.log('attempted to load hello.html')
+    next()
+}, (req, res) => {
+    res.send('Hello Rasta!')
+})
+
+//Chaining route handlers
+const one = (req, res, next) => {
+    console.log('one')
+    next();
+}
+const two = (req, res, next) => {
+    console.log('two')
+    next();
+}
+const three = (req, res) => {
+    console.log('three')
+    res.send('Finished')
+}
+
+app.get(['/chain', 'chain.html'], [one, two, three]);
+
+
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
